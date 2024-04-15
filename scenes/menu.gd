5a40100eb0619a85fa574681055f6b3c9c44f456
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2024-04-15 19:20:00 -0300

    prototipo

---
+++ scenes/menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_options_button_pressed():
+	pass
+	#var options = load("res://icon.svg").instance()
+	#get_tree().current_scene.add_child(options)
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/creditos.tscn")
+
+func _on_extras_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	get_tree().quit()
