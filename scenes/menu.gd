c5a9739b7540e1c79810a2ae8d12c9c2e3080b42
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2024-05-02 15:35:42 -0300

    prototipo nivel

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_level.tscn")
 
 
 func _on_options_button_pressed():
